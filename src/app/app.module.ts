fca9cea3f77994a61416cf674c321df1fce3cc14
Zirui Liu <zirui.jerry@yahoo.com>
2024-02-12 20:41:04 -0600

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontComponent } from './front/front.component';
+
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { LtreeComponent } from './ltree/ltree.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontComponent,
-    LtreeComponent
+    FrontComponent
   ],
   imports: [
     BrowserModule,
