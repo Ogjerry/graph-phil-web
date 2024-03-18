17c6d356404678c86f8c4fa0c343ada7bb3d91e2
Zirui Liu <zirui.jerry@yahoo.com>
2024-03-18 15:49:51 -0500

    newf

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// This file is simply for git experiments
-
-function scrollToFigure(id) {
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-}
-
-// Add any additional JavaScript if needed
